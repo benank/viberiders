727995491aeb915a9dd030787cf283406b28a1d8
benank <gh@benank.com>
2025-03-18 10:38:34 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '4rem',
+      fontWeight: 'bold'
+    }}>
+      <div className="glowing-text">
+        Vibe Riders
+      </div>
+    </div>
+  )
+}
+
+export default App
